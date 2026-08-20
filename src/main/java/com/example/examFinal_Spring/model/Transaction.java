a48d08725c5cc772e888a2feea54fb9b46deda34
RAZAFINDRAKOTO Andraina <hei.andraina.6@gmail.com>
2026-08-20 15:44:55 +0300

    feat(files) : implement attributes of Account and Transaction

--- src/main/java/com/example/examFinal_Spring/model/Transaction.java
+++ src/main/java/com/example/examFinal_Spring/model/Transaction.java
@@ -1,5 +1,17 @@
 package com.example.examFinal_Spring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@AllArgsConstructor
+@Getter
 public class Transaction {
-    
+    private String id;
+    private Instant createdAt;
+    private TransactionType transactionType;
+    private BigDecimal amount;
+    private String reason;
 }
